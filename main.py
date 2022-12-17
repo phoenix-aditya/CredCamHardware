6243b77db6baddbdf9012c981cea6d4f6fe6d76a
Aditya Ranaut <ranout.aditya@gmail.com>
2022-12-17 23:42:59 +0530

    update

--- main.py
+++ main.py
@@ -2,25 +2,22 @@ from image_capture import capture_image
 from image_upload import upload_data_to_mongoDB, upload_image_to_ipfs
 from gpiozero import Button
 import os
+from signal import pause
 
 def click_image():
+    print('image capture begins')
     img_path = capture_image()
     upload_data_to_mongoDB(
         image_path=img_path
         )
     if os.path.exists("test.jpg"):
         os.remove("test.jpg")
+    print('image capture ends')
 
 # camera click button GPIO pin 22
-button = Button(22)
+button = Button(2)
+button.when_pressed = click_image
+pause()
 
-button.wait_for_press()
-print('button pressed')
-
-# while True:
-#     if button.is_pressed:
-#         print("Button is pressed")
-#         # click_image()
-#         print("image upload complete")
 
 
