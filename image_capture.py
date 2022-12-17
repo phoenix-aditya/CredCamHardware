51b4aa563e3bc15666d2c6b9e53d12472c397ac9
Aditya Ranaut <ranout.aditya@gmail.com>
2022-12-18 00:32:04 +0530

    updated file paths

--- image_capture.py
+++ image_capture.py
@@ -4,5 +4,5 @@ def capture_image() -> str:
     '''
     function takes an image in raspberry pi and returns the path to the image
     '''
-    os.system("raspistill -o test.jpg")
-    return "test.jpg"
+    os.system("raspistill -o /home/pi/CredCamHardware/test.jpg")
+    return "/home/pi/CredCamHardware/test.jpg"
