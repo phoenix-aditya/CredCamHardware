4796b8b7ef39e97bcfe52ec6041a5265d8301bc7
Aditya Ranaut <ranout.aditya@gmail.com>
2022-12-17 23:29:47 +0530

    added button script

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from image_capture import capture_image
 from image_upload import upload_data_to_mongoDB, upload_image_to_ipfs
+from gpiozero import Button
+import os
 
 def click_image():
     img_path = capture_image()
     upload_data_to_mongoDB(
         image_path=img_path
         )
+    if os.path.exists("test.jpg"):
+        os.remove("test.jpg")
 
-click_image()
+# camera click button GPIO pin 22
+button = Button(22)
+
+while True:
+    if button.is_pressed:
+        print("Button is pressed")
+        click_image()
+        print("image upload complete")
+
+
