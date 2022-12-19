f79f2478f7245b6b3cbe236d753aa00f34c5aed5
Aditya Ranaut <ranout.aditya@gmail.com>
2022-12-19 06:39:56 +0530

    Added changes

--- image_upload.py
+++ image_upload.py
@@ -4,21 +4,27 @@ import bson
 from bson.binary import Binary
 from datetime import datetime
 import os
+import requests
 
-def upload_image_to_ipfs():
-    return ""
+def upload_image_to_ipfs(files):
+    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files)
+    p = response.json()
+    hash = p['Hash']
+    return hash
 
 def upload_data_to_mongoDB(
     image_path:str,
     ):
     collection = database.images
     with open(image_path, "rb") as f:
+        hash = upload_image_to_ipfs(f.read())
         encoded = Binary(f.read())
 
     collection.insert_one(
         {
             "username":"aditya" , 
             "file": encoded, 
+            "hash": hash,
             "image_capture_time": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
             "location": "patiala"
         }
